bb963cc344e7159c30ae9d0106401ca34a11103b
Ignacio <as.ignasi97@gmail.com>
2021-04-28 14:10:20 +0200

    perfect

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,19 @@
 fun main() {
     val NNUMEROS = 99
+    val MINALEATORIO = 0
+    val MAXALEATORIO = 100
 
     var suma = 0
     var media = 0
     val lista = mutableListOf<Int>()
 
     for(i in 0..NNUMEROS){
-        lista.add((0..100).random())
+        lista.add((MINALEATORIO..MAXALEATORIO).random())
         suma += lista[i]
     }
 
-    media = suma/NNUMEROS
+    media = suma/(NNUMEROS+1)
+    println("Media: $media")
 
     lista.forEach {
         if(it >= media)
